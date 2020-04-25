3f0eb4f6605fd09790bbe30e7800e78ed6f44d85
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-04-25 10:59:11 +0530

    Feature added to validate user mail id

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,15 +1,39 @@
 echo "welcome";
+    
+NAME_PATTERN="^[[:upper:]]{1}[[:lower:]]{2,}$";
+MAIL_PATTERN="^[a-zA-Z0-9$&+_-]+(\.[a-zA-Z0-9]+)*@([a-z0-9]+([a-z0-9-]*)\.)+[a-z]{2,4}$";
 
-echo "enter your first name ";read firstName;
-pattern="^[[:upper:]]{1}[[:lower:]]{2,}$";
-if [[ $firstName =~ $pattern ]]; then
-	echo "first name valid";
-    echo "enter your last name ";read lastName;
-    if [[ $lastName =~ $pattern ]]; then
-	    echo "last name valid";
+
+function takeFirstName() {
+    echo "enter your first name ";read firstName;
+    if [[ $firstName =~ $NAME_PATTERN ]]; then
+        echo "first name valid";
+        takeLastName;
     else 
-	    echo "last name not valid";
+        echo "first name not valid";
+        takeFirstName;
     fi
-else 
-	echo "first name not valid";
-fi
+}
+
+function  takeLastName(){
+    echo "enter your last name ";read lastName;
+    if [[ $lastName =~ $NAME_PATTERN ]]; then
+        echo "last name valid";
+        takeEmailId;
+    else 
+        echo "last name not valid";
+        takeLastName;
+    fi
+}
+
+function takeEmailId() {
+    echo "enter your mail id ";read mailId;
+    if [[ $mailId =~ $MAIL_PATTERN ]]; then
+        echo "mail id valid";
+    else 
+        echo "fmail id not valid";
+        takeEmailId;
+    fi
+}
+
+takeFirstName;
