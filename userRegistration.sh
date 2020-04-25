eff5c9dfd20cfe9b2d285e985554b534c4313a9c
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-04-25 11:27:07 +0530

    Code refactored

--- userRegistration.sh
+++ userRegistration.sh
@@ -4,49 +4,43 @@ NAME_PATTERN="^[[:upper:]]{1}[[:lower:]]{2,}$";
 MAIL_PATTERN="^[a-zA-Z0-9$&+_-]+(\.[a-zA-Z0-9]+)*@([a-z0-9]+([a-z0-9-]*)\.)+[a-z]{2,4}$";
 MOBILE_NUMBER_PATTERN="^[1-9]{2}[[:space:]]{1}[0-9]{10}$";
 
-function takeFirstName() {
-    echo "enter your first name ";read firstName;
-    if [[ $firstName =~ $NAME_PATTERN ]]; then
-        echo "first name valid";
-        takeLastName;
+function validate() {
+	read value
+	pattern=$1;
+	if [[ $value =~ $pattern ]]; then
+        echo "data valid";
     else 
-        echo "first name not valid";
-        takeFirstName;
+        echo "data not valid -- please enter valid data";
+        validate $pattern;
     fi
+
+}
+
+function takeFirstName() {
+    echo "enter your first name ";
+    validate $NAME_PATTERN; 
 }
 
 function  takeLastName(){
-    echo "enter your last name ";read lastName;
-    if [[ $lastName =~ $NAME_PATTERN ]]; then
-        echo "last name valid";
-        takeEmailId;
-    else 
-        echo "last name not valid";
-        takeLastName;
-    fi
+    echo "enter your last name ";    
+    validate $NAME_PATTERN; 
 }
 
 function takeEmailId() {
-    echo "enter your mail id ";read mailId;
-    if [[ $mailId =~ $MAIL_PATTERN ]]; then
-        echo "mail id valid";
-        takeMobileNumber;
-    else 
-        echo "mail id not valid";
-        takeEmailId;
-    fi
+    echo "enter your mail id ";  
+    validate  $MAIL_PATTERN; 
 }
 
 function takeMobileNumber() {
-    echo "enter your mobile number ";read mobileNumber;
-    if [[ $mobileNumber =~ $MOBILE_NUMBER_PATTERN ]]; then
-        echo "mobile number valid";
-    else 
-        echo "mobile number not valid";
-        takeMobileNumber;
-    fi
+    echo "enter your mobile number ";
+    validate $MOBILE_NUMBER_PATTERN; 
 }
 
+function takeData() {
+	takeFirstName;
+	takeLastName;
+	takeEmailId;
+    takeMobileNumber;
+}
 
-
-takeFirstName;
+takeData;
