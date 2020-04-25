6adaa65c87f054a217d56b01bc2f1f267b4f5fc1
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-04-25 10:49:01 +0530

    Feature added to validate last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,9 +1,15 @@
 echo "welcome";
 
-echo "enter your first name ";read name;
+echo "enter your first name ";read firstName;
 pattern="^[[:upper:]]{1}[[:lower:]]{2,}$";
-if [[ $name =~ $pattern ]]; then
-	echo "valid";
+if [[ $firstName =~ $pattern ]]; then
+	echo "first name valid";
+    echo "enter your last name ";read lastName;
+    if [[ $lastName =~ $pattern ]]; then
+	    echo "last name valid";
+    else 
+	    echo "last name not valid";
+    fi
 else 
-	echo "not valid";
+	echo "first name not valid";
 fi
