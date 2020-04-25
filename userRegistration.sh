6b337f2960551d0cbe37f6d051f47a6add2324bd
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-04-25 10:40:48 +0530

    Feature added to validate first name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,1 +1,9 @@
 echo "welcome";
+
+echo "enter your first name ";read name;
+pattern="^[[:upper:]]{1}[[:lower:]]{2,}$";
+if [[ $name =~ $pattern ]]; then
+	echo "valid";
+else 
+	echo "not valid";
+fi
